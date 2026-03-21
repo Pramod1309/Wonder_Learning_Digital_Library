12cfd46999d61126948e82dfa24684a09ee47465
Pramod1309 <pramodjadhav1876@gmail.com>
2026-03-21 15:56:15 +0530

    initial setup

---
+++ backend/database.py
@@ -0,0 +1,7 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "postgresql://pramod:Pramod%4013091876@localhost/digital_library"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
